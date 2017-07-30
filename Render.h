951b2b2ed233daa3c89876e5230a9b368796319d
QuantumAnon <QuantumAnon@users.noreply.github.com>
2017-07-30 23:04:08 +0100

    Rebased render branch to include issue fixes

---
+++ Render.h
@@ -0,0 +1,14 @@
+//========================================
+//Contains render functions for bitmaps
+//----------------------------------------
+//
+//========================================
+
+#pragma once
+
+#include "stdio.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <termios.h>
+#include <unistd.h>
