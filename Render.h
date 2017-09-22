ee5e19de3374c0cf6b33ed5e5ba890f6f6e992fa
QuantumAnon <QuantumAnon@users.noreply.github.com>
2017-09-22 13:43:14 +0100

    image rendering now works

--- Render.h
+++ Render.h
@@ -1,16 +1,15 @@
+#ifndef Render_h
+#define Render_h
+
 //========================================
 //Contains render functions for bitmaps
 //----------------------------------------
 //
 //========================================
-
-#pragma once
-
 #include "stdio.h"
 #include "png.h"
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <stdint.h>
 
 //A single pixel with an RGB value
@@ -31,7 +30,7 @@ typedef struct
 } canvas_t;
 
 //Find the pixel data at a specific position
-static pixel_t *pixel_at(canvas_t *canvas, int x, int y)
+static pixel_t *pixel_at_canvas(canvas_t *canvas, int x, int y)
 {
   if ((x >= canvas->width) || (y >= canvas->height)) { //check if pixel requested is within canvas size
     return NULL;
@@ -39,6 +38,14 @@ static pixel_t *pixel_at(canvas_t *canvas, int x, int y)
     return (canvas->canvas_start + x + y * canvas->width);
   }
 }
+
+void newCanvas(canvas_t *canvas, int width, int height)
+{
+  canvas->width = width;
+  canvas->height = height;
+  canvas->canvas_start = (pixel_t*)calloc(canvas->height*canvas->width, sizeof(pixel_t));
+}
+
 
 //Paint a row with a particular array of values, naively stretches a smaller array to fit the size needed
 void paint(canvas_t *canvas, pixel_t values[], int row, int width)
@@ -50,24 +57,32 @@ void paint(canvas_t *canvas, pixel_t values[], int row, int width)
   int h_size = canvas->width / width;
 
   int cur = 0;
-  for (int i = 0; i < canvas->width; i += h_size) {
+  for (int i = 0; i < width; i++) {
     for (int j = 0; j < h_size; j++) {
-      pixel_at(canvas, i + j, row)->red = values[cur].red;
-      pixel_at(canvas, i + j, row)->green = values[cur].green;
-      pixel_at(canvas, i + j, row)->blue = values[cur].blue;
-      pixel_at(canvas, i + j, row)->alpha = values[cur].alpha;
+      pixel_at_canvas(canvas, i*h_size + j, row)->red   = values[cur].red;
+      pixel_at_canvas(canvas, i*h_size + j, row)->green = values[cur].green;
+      pixel_at_canvas(canvas, i*h_size + j, row)->blue  = values[cur].blue;
+      pixel_at_canvas(canvas, i*h_size + j, row)->alpha = values[cur].alpha;
     }
     cur++;
+  }
+
+  for (int i = cur * h_size; i < canvas->width; i++) {
+    pixel_at_canvas(canvas, i, row)->red   = 0;
+    pixel_at_canvas(canvas, i, row)->green = 0;
+    pixel_at_canvas(canvas, i, row)->blue  = 0;
+    pixel_at_canvas(canvas, i, row)->alpha = 0;
   }
 }
 
 //Convert a canvas to an RGBA pixel array
-void rasterize(canvas_t *canvas, unsigned char *image, unsigned int block_width, unsigned int block_height)
+void rasterize(canvas_t *canvas,  canvas_t *image, size_t block_width, size_t block_height)
 {
   //allocate memory
-  image = (unsigned char*)calloc(canvas->width * canvas->height * block_width * block_height * 4, sizeof(unsigned));
+  image->canvas_start = (pixel_t*)calloc(canvas->width * canvas->height * block_width * block_height, sizeof(pixel_t));
   //image height and width
-  int img_height = canvas->height * block_height, img_width = canvas->width * block_width;
+  image->height = canvas->height * block_height, image->width = canvas->width * block_width;
+
 
   //left->right, top->down across blocks
   for (int y = 0; y < canvas->height; y++) {
@@ -78,19 +93,16 @@ void rasterize(canvas_t *canvas, unsigned char *image, unsigned int block_width,
       unsigned anchor_x = x * block_width;
 
       //pixel to represent
-      pixel_t cur = *pixel_at(canvas, x, y);
+      pixel_t cur = *pixel_at_canvas(canvas, x, y);
 
       //left->right, top->down across a single block
       for (int j = 0; j < block_height; j++) {
         for (int i = 0; i < block_width; i++) {
-          //pixel_coord = (i+anchor_x, j+anchor_y)
-          unsigned pixel_number = (i+anchor_x) + (j + anchor_y)*img_width;
-
           //assign values
-          image[4*pixel_number + 0] = cur.red;
-          image[4*pixel_number + 1] = cur.green;
-          image[4*pixel_number + 2] = cur.blue;
-          image[4*pixel_number + 3] = cur.alpha;
+          pixel_at_canvas(image, anchor_x + i, anchor_y + j)->red   = cur.red;
+          pixel_at_canvas(image, anchor_x + i, anchor_y + j)->green = cur.green;
+          pixel_at_canvas(image, anchor_x + i, anchor_y + j)->blue  = cur.blue;
+          pixel_at_canvas(image, anchor_x + i, anchor_y + j)->alpha = cur.alpha;
         }
       }
     }
@@ -98,14 +110,14 @@ void rasterize(canvas_t *canvas, unsigned char *image, unsigned int block_width,
 }
 
 //write a canvas to file
-int writeImage(char* filename, unsigned int width, unsigned int height, unsigned char* image, char* title)
+int writeImage(char* filename, canvas_t *image, char* title)
 {
   int code = 0;
   FILE *fp = NULL;
   png_structp png_ptr = NULL;
   png_infop info_ptr = NULL;
   png_bytep row = NULL;
-  
+
   // Open file for writing (binary mode)
   fp = fopen(filename, "wb");
   if (fp == NULL) {
@@ -129,7 +141,7 @@ int writeImage(char* filename, unsigned int width, unsigned int height, unsigned
      code = 1;
      goto finalise;
   }
-  
+
   // Setup Exception handling
   if (setjmp(png_jmpbuf(png_ptr))) {
      fprintf(stderr, "Error during png creation\n");
@@ -140,7 +152,7 @@ int writeImage(char* filename, unsigned int width, unsigned int height, unsigned
   png_init_io(png_ptr, fp);
 
   // Write header (8 bit colour depth)
-  png_set_IHDR(png_ptr, info_ptr, width, height,8 , PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+  png_set_IHDR(png_ptr, info_ptr, image->width, image->height, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 
   // Set title
   if (title != NULL) {
@@ -151,22 +163,25 @@ int writeImage(char* filename, unsigned int width, unsigned int height, unsigned
      png_set_text(png_ptr, info_ptr, &title_text, 1);
   }
 
+  png_set_invert_alpha(png_ptr);
   png_write_info(png_ptr, info_ptr);
 
   // Allocate memory for one row (4 bytes per pixel - RGBA)
-  row = (png_byte*)calloc(4 * width, sizeof(png_byte));
+  row = (png_byte*)calloc(image->width, sizeof(pixel_t));
 
   // Write image data
   int x, y;
-  for (y=0 ; y < height ; y++) {
-     for (x=0 ; x < width ; x++) {
-        row[x*4 + 0] = image[x + y * width + 0];
-        row[x*4 + 1] = image[x + y * width + 1];
-        row[x*4 + 2] = image[x + y * width + 2];
-        row[x*4 + 3] = image[x + y * width + 3];
+  for (y=0 ; y < image->height ; y++) {
+     for (x=0 ; x < image->width ; x++) {
+        pixel_t cur = *pixel_at_canvas(image, x, y);
+
+        row[x*4 + 0] = cur.red;
+        row[x*4 + 1] = cur.blue;
+        row[x*4 + 2] = cur.green;
+        row[x*4 + 3] = cur.alpha;
      }
-     printf("row %d\n",y);
      png_write_row(png_ptr, row);
+     //printf("at row %d\n", y);
   }
 
   // End write
@@ -178,8 +193,6 @@ int writeImage(char* filename, unsigned int width, unsigned int height, unsigned
 
   png_write_end(png_ptr, NULL);
 
-  printf("snoot \n");
-
   finalise:
   if (fp != NULL) fclose(fp);
   if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
@@ -188,3 +201,5 @@ int writeImage(char* filename, unsigned int width, unsigned int height, unsigned
 
   return code;
 }
+
+#endif
