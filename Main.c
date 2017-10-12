7bddc3754f2f5f2fbf89d6979123c55e57673ae7
QuantumAnon <QuantumAnon@users.noreply.github.com>
2017-10-12 17:06:07 +0100

    rendering now semi works

--- Main.c
+++ Main.c
@@ -39,32 +39,66 @@ list Kolakoski(int n)
 
 int main( int argc, const char* argv[] )
 {
-  pixel_t *pixels[100];
-  char *output = "test.png";
-  image_t canvas;
-  image_t image;
-  newImage(&canvas, 200, 100);
+  //init variables
+  int rndr_w, rndr_h, blck_w, blck_h;
+  float blankTolerance;
 
-  for (int i = 0; i <= 100; i++) {
-    list K = Kolakoski(i + 3);
-    int n = Size(&K);
-    printf("%i -- %i\n", i + 3, n);
+
+
+  rndr_w = 10000;
+  rndr_h = 6;
+  blck_w = 1;
+  blck_h = 500;
+  blankTolerance = 0.1;
+
+  //jagged array representing actual pixels
+  pixel_t *pixels[rndr_h];
+
+  //filename
+  char *output = "test.png";
+
+  //canvas to draw on
+  image_t canvas;
+  //image to render
+  image_t image;
+
+  //canvas is rndr_w x rndr_h
+  newImage(&canvas, rndr_w, rndr_h);
+
+  int oldLength = 0;
+
+  //from 0 to the height of the render
+  for (int i = 0; i < rndr_h; i += 1) {
+    int c = i - 1;
+    list K;
+    int n;
+
+    do {
+      c++;
+      //generate Kolakoski sequence using c iterations
+      K = Kolakoski(c);
+      n = Length(&K);
+    } while ((n % rndr_w > blankTolerance * rndr_w) || (n < 2 * oldLength));
+    oldLength = n;
+    
+    //printf("%i -- %i\n", i + 3, n);
     int p[n];
     Array(p, &K);
     //int max = (n < 150) ? n : 50;
     pixels[i] = (pixel_t*)calloc(n,sizeof(pixel_t));
 
-    for (int j = 0; j < n; j++) {
-      if (p[j] == 1) {
-        pixel_set(&pixels[i][j], 200, 0, 0, 0);
+    for (int k = 0; k < n; k++) {
+      if (p[k] == 1) {
+        pixel_set(&pixels[i][k], 200, 0, 0, 0);
       } else {
-        pixel_set(&pixels[i][j], 0, 0, 200, 0);
+        pixel_set(&pixels[i][k], 0, 0, 200, 0);
       }
-    }
-    paint(&canvas, pixels[i], n, 100-i);
+  }
+    paint(&canvas, pixels[i], n, rndr_h - i);
   }
 
-  rasterize(&canvas, &image, 10, 20);
+
+  rasterize(&canvas, &image, blck_w, blck_h);
 
   writeImage(output, &image, "test");
 
